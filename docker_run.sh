d6c503ab9e5ead77e9c41dd23f0dceff9edbf8e0
Martin Raiber <martin@urbackup.org>
2020-02-15 10:12:38 -0500

    Bind to localhost only

--- docker_run.sh
+++ docker_run.sh
@@ -7,4 +7,4 @@ CNAME="installer-creator1"
 docker stop "$CNAME" || true
 docker rm "$CNAME" || true
 
-docker run -d -p 5000:5000 --name "$CNAME" installer_creator
+docker run -d -p 127.0.0.1:5000:5000 --name "$CNAME" installer_creator
